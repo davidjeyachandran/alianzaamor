5b34febc15103f41d262c4f51aa699b92540e374
VladimirAus <v_roudakov@yahoo.com>
2020-05-29 10:52:31 +1000

    [#54] Refactored aa_core to use with multiple fields.

--- web/modules/custom/aa_core/src/Controller/DeliveryController.php
+++ web/modules/custom/aa_core/src/Controller/DeliveryController.php
@@ -24,14 +24,15 @@ class DeliveryController extends ControllerBase {
    *
    * @param \Drupal\node\NodeInterface $node
    *   The node to export.
-   *
    * @param \Drupal\user\UserInterface $user
    *   The account for which a personal contact form should be generated.
+   * @param string $field_name
+   *   Field name to update: field_delivered, field_users_check_in or field_users_opt_out.
    *
    * @return array
    *   A render array representing the administrative page content.
    */
-  public function toDeliver(NodeInterface $node, UserInterface $user) : array {
+  public function updateDelivery(NodeInterface $node, UserInterface $user, $field_name = 'field_delivered') : array {
 
     // @TODO: Improve below logic.
     // Move to access node class.
@@ -41,13 +42,19 @@ class DeliveryController extends ControllerBase {
     if (!$user->isAuthenticated()) {
       throw new NotFoundHttpException();
     }
+
+    // Check that correct field is used.
+    if (in_array($field_name, ['field_delivered', 'field_users_check_in', 'field_users_opt_out'])) {
+      throw new NotFoundHttpException;
+    }
+
     // Route for redirecting back.
     $route = Url::fromRoute('view.miembros.page_users_to_deliver', [
       'node' => $node->id(),
     ]);
     $response = new RedirectResponse($route->toString());
 
-    $users = $node->field_delivered->referencedEntities();
+    $users = $node->$field_name->referencedEntities();
     $users_found = array_filter($users, static function (UserInterface $user_reference) use ($user) {
       return $user_reference->id() === $user->id();
     });
@@ -62,7 +69,7 @@ class DeliveryController extends ControllerBase {
 
     // Adding a user if delivered food.
     try {
-      $node->field_delivered[] = $user->id();
+      $node->$field_name[] = $user->id();
       $node->save();
 
       // In Spanish so far since we are doing for spanish.
